e1ce8622225435c517d44fd97ea1fd7d25ef34c4
jeffrey rios jr <jeffriosjr@gmail.com>
2023-01-13 00:08:26 -0800

    updated squares and media queries

--- homepage.php
+++ homepage.php
@@ -16,21 +16,22 @@
 get_header();
 ?>
 
-<main id="primary" class="site-main square-main container-fluid">
-	<div class ="container-fluid ">
-		<header class="m-5 p-5">
+<main id="primary" class="site-main square-main">
+	<div class ="container-fluid">
+		<header class="m-1 p-1">
 				<h1 class="page-title m-5"><?php single_post_title();?>
 				The-Jeffrey-Squares
 				</h1>
 		</header>
-		<div id="container"></div>
+		<div id="jeffsquares"></div>
 	</div>
 </main>
 
 
 
 <script>
-    const container = document.getElementById("container");
+
+const jeffsquares = document.getElementById("jeffsquares");
 
 let buttons = [];
 let inputs = [];
@@ -54,8 +55,8 @@ for (let i = 0; i < 100; i++) {
   // Append the number element to the square
   square.appendChild(number);
 
-  // Append each square to the container element
-  container.appendChild(square);
+  // Append each square to the jeffs-squares element
+  jeffsquares.appendChild(square);
 
   // Add an event listener to each square that listens for a click event
   square.addEventListener("click", handleSquareClick);
@@ -69,55 +70,42 @@ function handleSquareClick() {
   }
   // create input and button element
   const input = document.createElement("input");
+  input.setAttribute("maxlength", "12");
   const submitBtn = document.createElement("button");
   // set button text
-  submitBtn.textContent = "Submit";
+  submitBtn.textContent = "confirm";
   //append input and button to square
   this.appendChild(input);
   this.appendChild(submitBtn);
-  
-  // Create the disappear button
-  const disappearBtn = document.createElement("button");
-  disappearBtn.textContent = "Thanks my GUY!";
-  disappearBtn.onclick = function() {
-    this.style.display = "none";
+
+  // Create the unclick button
+  const unclickBtn = document.createElement("button");
+  unclickBtn.textContent = "Unclick";
+  unclickBtn.onclick = function() {
+    handleUnclick(input, this.parentNode, submitBtn);
   };
-  this.appendChild(disappearBtn);
+  this.appendChild(unclickBtn);
   
   // keep track of input and button element
   inputs.push(input);
   buttons.push(submitBtn);
   // Add an event listener to the submit button that listens for a click event
-  submitBtn.addEventListener("click", () => handleButtonClick(input, this, submitBtn, disappearBtn));
+  submitBtn.addEventListener("click", () => handleButtonClick(input, this, submitBtn, unclickBtn));
 }
 
-
-function handleButtonClick(input, square, button) {
-  //get input value
-  const name = input.value;
-  if (!validateInput(input)) {
-    //display error message and return;
-    return;
-  }
-  // create text node
-  const textNode = document.createTextNode(name);
-  // append text node to square
-  square.appendChild(textNode);
+function handleUnclick(input, square, submitBtn) {
   // remove input and button
-  square.removeChild(inputs[inputs.indexOf(input)]);
-  square.removeChild(buttons[buttons.indexOf(button)]);
-  square.classList.add("square-filled");
-  square.filled = true;
-  square.removeEventListener("click", handleSquareClick);
-  if (document.querySelectorAll(".square-filled").length === 100) {
-    //save feature code here
-  }
+  square.removeChild(input);
+  square.removeChild(submitBtn);
+  square.removeChild(unclickBtn);
+  square.classList.remove("square-filled");
+  square.filled = false;
+  square.addEventListener("click", handleSquareClick);
 }
+
 
 
 </script>
 
 <?php
 get_footer();
-
-  
